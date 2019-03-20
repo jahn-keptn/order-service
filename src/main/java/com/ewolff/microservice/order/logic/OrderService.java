d83eaadf422fe5e26a8ef0b61e3cf9b983993151
EC2 Default User <ec2-user@ip-172-31-57-201.ec2.internal>
2019-03-20 20:05:47 +0000

    added fib to OrderService.java

--- src/main/java/com/ewolff/microservice/order/logic/OrderService.java
+++ src/main/java/com/ewolff/microservice/order/logic/OrderService.java
@@ -29,6 +29,9 @@ class OrderService {
 		if (!customerClient.isValidCustomerId(order.getCustomerId())) {
 			throw new IllegalArgumentException("Customer does not exist!");
 		}
+    		for(int i=1; i<=100000; i++){
+      			fibonacci2(i);
+   		}
 		return orderRepository.save(order);
 	}
 
@@ -36,4 +39,24 @@ class OrderService {
 		return orderRepository.findById(orderId).get().totalPrice(itemClient);
 	}
 
+  	public int fibonacci(int number){
+   	 	if(number == 1 || number == 2){
+     	 		return 1;
+    		}
+   		return fibonacci(number-1) + fibonacci(number -2); //tail recursion
+ 	}
+
+  	public int fibonacci2(int number){
+    		if(number == 1 || number == 2) {
+      			return 1;
+    		}
+    		int fibo1=1, fibo2=1, fibonacci=1;
+    		for(int i= 3; i<= number; i++){
+      			//Fibonacci number is sum of previous two Fibonacci number
+      			fibonacci = fibo1 + fibo2;
+      			fibo1 = fibo2;
+      			fibo2 = fibonacci;
+    		}
+    		return fibonacci; //Fibonacci number
+  	}
 }
