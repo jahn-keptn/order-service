d07207a839e1569282b19256bfaa10e7bbd1a74a
Andreas Grabner <grabnerandi@gmx.at>
2019-03-21 15:13:29 +0100

    Adding an arfitical slowdown

--- src/main/java/com/ewolff/microservice/order/logic/OrderController.java
+++ src/main/java/com/ewolff/microservice/order/logic/OrderController.java
@@ -48,6 +48,11 @@ class OrderController {
 
 	@RequestMapping("/")
 	public ModelAndView orderList() {
+		// Adding a slowdown by calling a fibunacci function 10000 times
+		for(int i=1; i<=100000; i++){
+      			fibonacci2(i);
+   		}
+		
 		return new ModelAndView("orderlist", "orders",
 				orderRepository.findAll());
 	}
@@ -81,4 +86,17 @@ class OrderController {
 		return new ModelAndView("success");
 	}
 
+	 public int fibonacci2(int number){
+    		if(number == 1 || number == 2) {
+      			return 1;
+    		}
+    		int fibo1=1, fibo2=1, fibonacci=1;
+    		for(int i= 3; i<= number; i++){
+      			//Fibonacci number is sum of previous two Fibonacci number
+      			fibonacci = fibo1 + fibo2;
+      			fibo1 = fibo2;
+      			fibo2 = fibonacci;
+    		}
+    		return fibonacci; //Fibonacci number
+  	}
 }
